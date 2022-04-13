72f473f4202af08581a220936c8582ee4d925fce
Htet Aung Khant <hakhant21@gmail.com>
2022-04-13 21:23:02 +0630

    update url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://www.hakhant.site',
     methods: ['GET', 'POST'],
   },
 });
