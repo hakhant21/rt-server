63c73674664aed9b4ca4a8cf1d76d1a5738b95fb
Htet Aung Khant <hakhant21@gmail.com>
2022-04-13 20:57:21 +0630

    socket server

--- index.js
+++ index.js
@@ -2,14 +2,35 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
-require('dotenv').config();
-
 const app = express();
 app.use(cors());
-
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 5000;
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on('send_message', (data) => {
+    socket.to(data.room).emit('receive_message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User Disconnected', socket.id);
+  });
+});
+
+const PORT = 3001;
 server.listen(PORT, () => {
-  console.log(`Socket server is running on port:${PORT}`);
+  console.log(`Socket server started on port ${PORT}`);
 });
